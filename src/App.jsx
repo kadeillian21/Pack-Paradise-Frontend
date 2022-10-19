2950b88f5693425303ab8e357beba19a4a7d96be
Kade Illian <kadeillianmt@gmail.com>
2022-10-19 12:00:35 -0600

    added a modal that will become our show action

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import { ProductsIndex } from "./ProductsIndex";
 import { ProductsNew } from "./ProductsNew";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Modal } from "./Modal";
 
 export function Home() {
   const [products, setProducts] = useState([]);
@@ -17,6 +18,9 @@ export function Home() {
     <div>
       <ProductsNew />
       <ProductsIndex products={products} />
+      <Modal show={true}>
+        <p>Test</p>
+      </Modal>
     </div>
   );
 }
