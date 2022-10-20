e9d6652dc1bc37c5c98ced9ad760bd72f18ae486
Kade Illian <kadeillianmt@gmail.com>
2022-10-20 13:26:19 -0600

    made it so that signup actually talks to our backend

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,8 +1,26 @@
+import axios from "axios";
+
 export function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit");
+    const params = new FormData(event.target);
+
+    axios
+      .post("http://localhost:3000/users", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div id="signup">
       <h1>Signup</h1>
-      <form method="POST" action="http://localhost:3000/users">
+      <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
         </div>
