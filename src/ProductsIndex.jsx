a2f97ad647e50c72873c37a30c0838a63f435ed9
Kade Illian <kadeillianmt@gmail.com>
2022-10-27 10:59:23 -0600

    implemented a search bar in the products index action

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
+
 export function ProductsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
   props.products;
   return (
     <div id="products-index">
       <h1>All Products</h1>
-      {props.products.map((product) => (
-        <div className="products" key={product.id}>
-          <h2>{product.name}</h2>
-          <img src={product.image}></img>
-          <p>${product.price}</p>
-          <p>{product.description}</p>
-          <button onClick={() => props.onSelectProduct(product)}>More info</button>
-        </div>
-      ))}
+      Search filter:{" "}
+      <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      {props.products
+        .filter((product) => product.name.toLowerCase().includes(searchFilter))
+        .map((product) => (
+          <div className="products" key={product.id}>
+            <h2>{product.name}</h2>
+            <img src={product.image}></img>
+            <p>${product.price}</p>
+            <p>{product.description}</p>
+            <button onClick={() => props.onSelectProduct(product)}>More info</button>
+          </div>
+        ))}
     </div>
   );
 }
